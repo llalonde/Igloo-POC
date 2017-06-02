a37bc73d7debe2772394a991b15c3ee060154880
Pierre Roman <pierrer@microsoft.com>
2017-06-02 17:09:22 -0400

    updates password

--- Deploy-ReferenceArchitecture.ps1
+++ Deploy-ReferenceArchitecture.ps1
@@ -8,7 +8,7 @@ $ErrorActionPreference = "Stop"
 
 # sign in
 Write-Host "Logging in...";
-Login-AzureRmAccount | Out-Null
+#Login-AzureRmAccount | Out-Null
 
 # select subscription
 $subscriptionId = Read-Host -Prompt 'Input your Subscription ID'
@@ -36,7 +36,7 @@ Write-Host
 # set  Root Uri of GitHub Repo (select AbsoluteUri)
 
 $TemplateRootUriString = "https://raw.githubusercontent.com/pierreroman/Igloo-POC/master/"
-$TemplateURI=New-Object System.Uri -ArgumentList @($TemplateRootUriString)
+$TemplateURI = New-Object System.Uri -ArgumentList @($TemplateRootUriString)
 
 # Templates for the deploment (include filename)
 
@@ -53,10 +53,13 @@ $ASAParametersFile = $TemplateURI.AbsoluteUri + "parameters/asa.parameters.json"
 
 # Create the resource group
 
-if ( -not $ResourceGroupName ) {
+Get-AzureRmResourceGroup -Name $ResourceGroupName -ev notPresent -ea 0  | Out-Null
+
+if ($notPresent) {
     Write-Output "Could not find resource group '$ResourceGroupName' - will create it"
     Write-Output "Creating resource group '$ResourceGroupName' in location '$Location'"
-    New-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location -Forcey
+    New-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location -Force | Out-Null
+
 }
 else {
     Write-Output "Using existing resource group '$ResourceGroupName'"
@@ -69,8 +72,7 @@ Write-Output "Starting deployment"
 #region Deployment of virtual network
 Write-Output "Deploying virtual network..."
 
-if (Invoke-WebRequest -Uri $VnetParametersFile)
-{
+if (Invoke-WebRequest -Uri $VnetParametersFile) {
     write-host "The parameter file was found, we will use the following info: "
     write-host " Template file:     '$VnetTemplate'"
     write-host " Parameter file:    '$VnetParametersFile'"
@@ -78,8 +80,7 @@ if (Invoke-WebRequest -Uri $VnetParametersFile)
 
     New-AzureRmResourceGroupDeployment -Mode Complete -Name "vnet-deployment" -ResourceGroupName $ResourceGroupName -TemplateUri $VnetTemplate -TemplateParameterUri $VnetParametersFile -Force | Out-Null
 }
-else
-{
+else {
     write-host "The parameter file was not found, you will need to enter all parameters manually...."
     write-host
     New-AzureRmResourceGroupDeployment -Mode Complete -Name "vnet-deployment" -ResourceGroupName $ResourceGroupName -TemplateUri $VnetTemplate -Force | Out-Null
@@ -87,22 +88,35 @@ else
 }
 
 #endregion
+
 Write-Output "Deploying Cisco ASAv appliance..."
 
-if (Invoke-WebRequest -Uri $ASAParametersFile)
-{
+$ASAResourceGroupName = $ResourceGroupName + "-ASA"
+
+Get-AzureRmResourceGroup -Name $ASAResourceGroupName -ev notPresent -ea 0
+
+if ($notPresent) {
+    Write-Output "Could not find resource group '$ASAResourceGroupName' - will create it"
+    Write-Output "Creating resource group '$ASAResourceGroupName' in location '$Location'"
+    New-AzureRmResourceGroup -Name $ASAResourceGroupName -Location $Location -Force | Out-Null
+
+}
+else {
+    Write-Output "Using existing resource group '$ASAResourceGroupName'"
+}
+
+if (Invoke-WebRequest -Uri $ASAParametersFile) {
+    
     write-host "The parameter file was found, we will use the following info: "
     write-host " Template file:     '$ASATemplate'"
     write-host " Parameter file:    '$ASAParametersFile'"
     write-host
-
-    New-AzureRmResourceGroupDeployment -Mode Complete -Name "vnet-deployment" -ResourceGroupName ($ResourceGroupName + "-ASA") -TemplateUri $ASATemplate -TemplateParameterUri $ASAParametersFile -Force | Out-Null
+    New-AzureRmResourceGroupDeployment -Mode Complete -Name "vnet-deployment" -ResourceGroupName $ASAResourceGroupName -TemplateUri $ASATemplate -TemplateParameterUri $ASAParametersFile -Force | Out-Null
 }
-else
-{
+else {
     write-host "The parameter file was not found, you will need to enter all parameters manually...."
     write-host
-    New-AzureRmResourceGroupDeployment -Mode Complete -Name "vnet-deployment" -ResourceGroupName ($ResourceGroupName + "-ASA") -TemplateUri $ASATemplate -Force | Out-Null
+    New-AzureRmResourceGroupDeployment -Mode Complete -Name "vnet-deployment" -ResourceGroupName $ASAResourceGroupName -TemplateUri $ASATemplate -Force | Out-Null
 
 }
 #region deployment of ASA firewall
