9aa5c0bd80ed22a67abd90a6e7705e28b50fecdb
Pierre Roman <pierrer@microsoft.com>
2017-06-02 15:19:23 -0400

    1

--- Deploy-ReferenceArchitecture.ps1
+++ Deploy-ReferenceArchitecture.ps1
@@ -4,6 +4,8 @@
 #******************************************************************************
 $ErrorActionPreference = "Stop"
 
+#region Prep & signin
+
 # sign in
 Write-Host "Logging in...";
 # Login-AzureRmAccount | Out-Null
@@ -27,6 +29,25 @@ Write-Host
 Write-Host "setting location as '$Location'";
 Write-Host 
 
+#endregion
+
+#region Set Template and Parameter location
+
+# set  Root Uri of GitHub Repo (select AbsoluteUri)
+
+$TemplateRootUriString = "https://raw.githubusercontent.com/pierreroman/Igloo-POC/master/"
+$TemplateURI=New-Object System.Uri -ArgumentList @($TemplateRootUriString)
+
+# Templates for the deploment (include filename)
+
+$VnetTemplate = $TemplateURI.AbsoluteUri + "vnet-subnet.json"
+
+#Parameter files for the deployment (include relative path to repo + filename)
+
+$VnetParametersFile = $TemplateURI.AbsoluteUri + "parameters/vnet-subnet.parameters.json"
+
+#endregion
+
 
 # Create the resource group
 
@@ -45,12 +66,37 @@ $ParametersFilePath = "C:\Users\pierrer\Documents\Github\Igloo-POC\parameters\vn
 
 # Start the deployment
 Write-Output "Starting deployment"
-if ( Test-Path $ParametersFilePath ) {
-    Write-Host "found parameter file..."
-    New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile $TemplateFilePath -TemplateParameterFile $ParametersFilePath | Out-Null
+
+
+#if ( Test-Path $ParametersFilePath ) {
+#    Write-Host "found parameter file..."
+#    New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile $TemplateFilePath -TemplateParameterFile $ParametersFilePath | Out-Null
+#}
+#else {
+#    Write-Host "Did not find parameter file..."
+#    New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile $TemplateFilePath  | Out-Null
+#}
+
+Write-Host "Deploying virtual network..."
+
+if (Invoke-WebRequest -Uri $VnetParametersFile)
+{
+    New-AzureRmResourceGroupDeployment -Name "vnet-deployment" -ResourceGroupName $networkResourceGroup.ResourceGroupName -TemplateUri $virtualNetworkTemplate -TemplateParameterFile $virtualNetworkParametersFile | Out-Null
 }
-else {
-    Write-Host "Did not find parameter file..."
-    New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile $TemplateFilePath  | Out-Null
+else
+{
+    write-host "The parameter file was not found, you will need to enter all parameters manually...."
+    write-host
+    New-AzureRmResourceGroupDeployment -Name "vnet-deployment" -ResourceGroupName $networkResourceGroup.ResourceGroupName -TemplateUri $virtualNetworkTemplate | Out-Null
+
 }
 
+#New-AzureRmResourceGroupDeployment -Name "vnet-deployment" -ResourceGroupName $networkResourceGroup.ResourceGroupName -TemplateUri $virtualNetworkTemplate -TemplateParameterFile $virtualNetworkParametersFile | Out-Null
+
+if ($error.Count -eq 0) {
+    Write-Host "Deployment of Vnet in Resource Group '$networkResourceGroupName' failed"
+}
+else {
+    Write-Host "Deployment of Vnet in Resource Group '$networkResourceGroupName' succeeded"
+}
+End region
