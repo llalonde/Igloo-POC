5297bbfbc78d902d9fc86b41bd81d5624675cc18
Pierre Roman <pierrer@microsoft.com>
2017-06-12 22:01:53 -0400

    change

---
+++ parameters/ImageLinuxVM.ps1
@@ -0,0 +1,3 @@
+﻿az vm deallocate --resource-group Igloo-POC-rg --name centos6temp2
+az vm generalize --resource-group Igloo-POC-rg --name centos6temp2
+az image create --resource-group Igloo-POC-rg --name Centos6 --source centos6temp2
