dff412dfea41f33709101375967baab7adec7785
Pierre Roman <pierrer@microsoft.com>
2017-06-12 23:42:19 -0400

    vm template

---
+++ PSFiles/vmcreationtest.ps1
@@ -0,0 +1,27 @@
+﻿#region Prep & signin
+# sign in
+Write-Host "Logging in ...";
+Login-AzureRmAccount | Out-Null
+
+# select subscription
+$subscriptionId = Read-Host -Prompt 'Input your Subscription ID'
+$Subscription = Select-AzureRmSubscription -SubscriptionId $SubscriptionId | out-null
+
+# select Resource Group
+$ResourceGroupName = Read-Host -Prompt 'Input the resource group for your network'
+
+# select Location
+$Location = Read-Host -Prompt 'Input the Location for your network'
+
+# select Location
+$VMListfile = Read-Host -Prompt 'Input the Location of the list of VMs to be created'
+
+#endregion
+
+#region Set Template and Parameter location
+# set  Root Uri of GitHub Repo (select AbsoluteUri)
+
+$TemplateRootUriString = "https://raw.githubusercontent.com/pierreroman/Igloo-POC/master/"
+$TemplateURI = New-Object System.Uri -ArgumentList @($TemplateRootUriString)
+
+$VnetTemplate = $TemplateURI.AbsoluteUri + "vnet-subnet.json"
