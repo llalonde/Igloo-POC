d529f7e0198c50560186ab6f3165f5769ccc55b4
Pierre Roman <pierrer@microsoft.com>
2017-06-14 12:54:04 -0400

    POSH

--- PSFiles/enable-posh-remote.ps1
+++ PSFiles/enable-posh-remote.ps1
@@ -1,1 +1,10 @@
-Enable-PSRemoting -Force
+$results = Enable-PSRemoting -Force
+
+$folder = "c:\temp"
+$log = "c:\temp\azurelog.txt"
+$date = Get-Date
+
+
+New-Item -Path $folder -ItemType Directory
+New-Item -Path $log -ItemType File
+Add-Content -Value " WinRM and Remote Powershell setup - $date $results" -Path $log
