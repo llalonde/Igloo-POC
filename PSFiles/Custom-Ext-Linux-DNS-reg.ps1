622eed48047b7192092275a54f62c4d4258de9d9
Pierre Roman <pierrer@microsoft.com>
2017-06-27 16:39:34 -0400

    runbooks

--- PSFiles/Custom-Ext-Linux-DNS-reg.ps1
+++ PSFiles/Custom-Ext-Linux-DNS-reg.ps1
@@ -1,26 +1,22 @@
 Login-AzureRmAccount
 
-$script = "https://poceussmb1.blob.core.windows.net/scripts/enable-posh-remote.ps1"
-$run="enable-posh-remote.ps1"
-$name="custom-script"
-$resourcegroup="igloo-POC-rg"
-$location="east us 2"
-
 $subscriptionId = Read-Host -Prompt 'Input your Subscription ID'
 $Subscription = Select-AzureRmSubscription -SubscriptionId $SubscriptionId | out-null
 
+
+#$script = '{"fileUris": ["https://raw.githubusercontent.com/pierreroman/Igloo-POC/master/Custom_Scripts/dnsupdate.sh"],"commandToExecute": "./dnsupdate.sh"}'
+$run="sh dnsupdate.sh"
+$name="customScript"
+$resourcegroup="igloo-POC-rg"
+$location="east us 2"
+
 $Vmlist = Get-AzureRmVM -ResourceGroupName $resourcegroup
 
-foreach ($vm in $Vmlist)
+foreach ($vm in ($Vmlist | where{$_.StorageProfile.OsDisk.OsType -eq 'Linux'}))
 {
-    if ($vm.StorageProfile.OsDisk.OsType -eq 'Windows')
-    {
-        $vmname = $vm.Name
-        Set-AzureRmVMCustomScriptExtension -ResourceGroupName $resourcegroup -VMName $vmname -Name $name -FileUri $script -Run $run -Location $location
-
-        Get-AzureRmVMCustomScriptExtension -ResourceGroupName $resourcegroup -VMName $vmname -Name $name
-
-    }
+    $vmname = $vm.Name.tostring()
+    Write-Output $vmname
+    #Get-AzureRmVMExtension -Name $name -ResourceGroupName $resourcegroup -VMName $vmname -Status
+    #Set-AzureRmVMCustomScriptExtension -ResourceGroupName $resourcegroup -VMName $vmname -Name $name -FileUri $script -Run $run -Location $location
+    az vm extension set --resource-group $resourcegroup --vm-name $vmname --name customScript --publisher Microsoft.Azure.Extensions --settings 
 }
-
-
