ea0676ec0579a9e2925705a25dd648f22c7fb286
Pierre Roman <pierrer@microsoft.com>
2017-06-27 22:12:08 -0400

    Linux custom script to register DNS

--- PSFiles/Custom-Ext-Linux-DNS-reg.ps1
+++ PSFiles/Custom-Ext-Linux-DNS-reg.ps1
@@ -3,10 +3,6 @@ Login-AzureRmAccount
 $subscriptionId = Read-Host -Prompt 'Input your Subscription ID'
 $Subscription = Select-AzureRmSubscription -SubscriptionId $SubscriptionId | out-null
 
-
-#$script = '{"fileUris": ["https://raw.githubusercontent.com/pierreroman/Igloo-POC/master/Custom_Scripts/dnsupdate.sh"],"commandToExecute": "./dnsupdate.sh"}'
-$run="sh dnsupdate.sh"
-$name="customScript"
 $resourcegroup="igloo-POC-rg"
 $location="east us 2"
 
@@ -16,7 +12,5 @@ foreach ($vm in ($Vmlist | where{$_.StorageProfile.OsDisk.OsType -eq 'Linux'}))
 {
     $vmname = $vm.Name.tostring()
     Write-Output $vmname
-    #Get-AzureRmVMExtension -Name $name -ResourceGroupName $resourcegroup -VMName $vmname -Status
-    #Set-AzureRmVMCustomScriptExtension -ResourceGroupName $resourcegroup -VMName $vmname -Name $name -FileUri $script -Run $run -Location $location
-    az vm extension set --resource-group $resourcegroup --vm-name $vmname --name customScript --publisher Microsoft.Azure.Extensions --settings 
+    az vm extension set --resource-group $resourcegroup --vm-name $vmname --name customScript --publisher Microsoft.Azure.Extensions --settings C:\Users\pierrer\Documents\Github\Igloo-POC\Custom_Scripts\linux-dns.json
 }
